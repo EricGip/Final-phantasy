3d7e249bef78d7e58931f8d524b4bd020904315d
eric <ericgip97@gmail.com>
2020-07-11 11:36:58 -0700

    added models and FORCE sync

---
+++ models/stats.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+    var Stats = sequelize.define("Stats", {
+      Gil: DataTypes.INTEGER,
+      Hp: DataTypes.INTEGER,
+      Attack: DataTypes.INTEGER,
+      Defense: DataTypes.INTEGER,
+      Speed: DataTypes.INTEGER,
+    });
+  
+    Stats.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      Stats.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Stats;
+  };
+  
