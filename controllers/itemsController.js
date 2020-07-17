60aeaef6e6c6295b9d8bf26946f0444f8e5d6b5a
Yev Ro <yev.rokhin@gmail.com>
2020-07-16 21:04:36 -0700

    working on retrieving user data...stats for stage1

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,28 +1,32 @@
 var express = require("express");
-
+const db = require("../models");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var items = require("../models/items.js");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.render("signup");
 })
 
-router.get("/login", function(req, res) {
+router.get("/login", function (req, res) {
     res.render("login")
 })
 
-router.get("/members", function(req, res) {
+router.get("/members", function (req, res) {
     res.render("members")
 })
 
-router.get('/store', function(req, res) {
+router.get('/store', function (req, res) {
     res.render("store")
 })
 
-router.get('/stage1', function(req, res) {
-    res.render("stage1")
+router.get('/stage1', function (req, res) {
+
+    db.Stats.findOne({ UserId: req.user.id }).then(stats => {
+
+        res.render("stage1", { stats: stats })
+    });
 })
 
 // Export routes for server.js to use.
