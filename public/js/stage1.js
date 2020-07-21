f3572bcd1061b0b04bbe5a0710611c9d0c8d2cba
Yev Ro <yev.rokhin@gmail.com>
2020-07-20 20:37:11 -0700

    stage1.js work - button clicks are listening - still waiting for functions to work

--- public/js/stage1.js
+++ public/js/stage1.js
@@ -1,63 +1,44 @@
-// import chocobo from "../assets/chocobo.png";
-// import orc from "../assets/orc.png";
-// import slime from "../assets/slime.png"
-
-//PULL CHARACTER STATS
-app.get("/api/user_stats", (req, res) => {
-  db.Stats.find({
-    UserID: req.user.id
-  }).then(results => {
-    console.log(results);
-  });
-});
-
-//action functions
-// $(document.readyState(function () {
-var Stats;
+var stats;
 $.ajax("api/user_stats").then(function (stats) {
-  Stats = Stats;
-
-  // const monsterImages = [ orc, slime, chocobo]
-  // $("#monster").attr("src", monsterImages[random]);
-  // console.log(monsterImages)
-
+  stats = stats;
 
   $(document).on("click", ".action", function (event) {
     var action = $(this).attr("data")
-    let user = Stats;
+    let user = stats;
+    console.log("clicked")
 
     switch (action) {
 
       case "attack":
         monster.hp -= user.attack - monster.defense;
-        $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
+        // $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
         user.hp -= monster.attack - user.defense;
-        $("#combat-log").append({ monster } + "hit you for" + { dmg } + "damage")
-
+        // $("#combat-log").append({ monster } + "hit you for" + { dmg } + "damage")
+        console.log("attack");
         break;
 
       case "guard":
         user.hp -= monster.attack - user.defense * 2;
-        $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
+        // $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
         monster.hp -= user.attack - monster.defense;
-        $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
-
+        // $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
+        console.log("guard");
         break;
 
       case "potion":
         user.hp += 20;
         user.potion -= 1;
-        $("#combat-log").append("you heal for 20 hp")
+        // $("#combat-log").append("you heal for 20 hp")
         user.hp -= monster.attack - user.defense;
-        $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
-
+        // $("#combat-log").append("you hit" + { monster } + "for" + { dmg } + "damage")
+        console.log("potion");
 
         break;
 
       case "run":
         alert("You are not fit to be an adventurer - RUN AWAY YOU COWARD!!11!!");
-        window.location();
-
+        window.open('', '_self').close();
+        console.log("run");
 
         break;
 
@@ -66,25 +47,5 @@ $.ajax("api/user_stats").then(function (stats) {
     }
   });
 
-  //attack!
-  function attack() {
-    // route to data base that retrieves the user's attack
-    //
-  }
-
-  //defend...does this reduce damage the user takes?
-  function guard() { }
-
-  //use a health pot?
-  function item() { }
-
-  //run away and we going to pop up a alert sating...you sissy...you ran away...game over?
-  function run() { }
-
-  //win or lose
-  function condition() {
-  };
-
 });
-// }));
 
